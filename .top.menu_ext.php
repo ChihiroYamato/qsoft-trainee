238501e3b1e55a0c05d3533bcfebc3054e5eef1a
alex_mironov <alexan9610@gmail.com>
2022-01-17 11:18:46 +0000

    Инициализация файлов

---
+++ .top.menu_ext.php
@@ -0,0 +1,21 @@
+<?php
+if (! defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) {die();}
+
+global $APPLICATION;
+
+// $aMenuLinksExt = $APPLICATION->IncludeComponent(
+//     "bitrix:menu.sections",
+//     "",
+//     Array(
+//         "CACHE_TIME" => "3600",
+//         "CACHE_TYPE" => "A",
+//         "DEPTH_LEVEL" => "1",
+//         "IBLOCK_ID" => "6",
+//         "IBLOCK_TYPE" => "products",
+//         "ID" => $_REQUEST["ID"],
+//         "IS_SEF" => "N",
+//         "SECTION_URL" => ""
+//     )
+// );
+
+// $aMenuLinks = array_merge($aMenuLinks, $aMenuLinksExt);
