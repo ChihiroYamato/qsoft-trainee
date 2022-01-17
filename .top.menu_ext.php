304660844401b00ac22cde0dbc0a38a07f39d959
alex_mironov <alexan9610@gmail.com>
2022-01-17 22:19:24 +0000

    Реализация шаблона меню

--- .top.menu_ext.php
+++ .top.menu_ext.php
@@ -3,19 +3,28 @@ if (! defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) {die();}
 
 global $APPLICATION;
 
-// $aMenuLinksExt = $APPLICATION->IncludeComponent(
-//     "bitrix:menu.sections",
-//     "",
-//     Array(
-//         "CACHE_TIME" => "3600",
-//         "CACHE_TYPE" => "A",
-//         "DEPTH_LEVEL" => "1",
-//         "IBLOCK_ID" => "6",
-//         "IBLOCK_TYPE" => "products",
-//         "ID" => $_REQUEST["ID"],
-//         "IS_SEF" => "N",
-//         "SECTION_URL" => ""
-//     )
-// );
+$aMenuLinksExt = $APPLICATION->IncludeComponent(
+    'bitrix:menu.sections',
+    '',
+    [
+        'CACHE_TIME' => '3600',
+        'CACHE_TYPE' => 'A',
+        'DEPTH_LEVEL' => '2',
+        'IBLOCK_ID' => '6',
+        'IBLOCK_TYPE' => 'products',
+        'IS_SEF' => 'N',
+        'SECTION_URL' => '',
+    ]
+);
 
-// $aMenuLinks = array_merge($aMenuLinks, $aMenuLinksExt);
+array_walk($aMenuLinksExt, function (&$menu) use ($aMenuLinks) {
+    foreach ($aMenuLinks as $item) {
+        if ($menu[3]['IS_PARENT'] && $menu[0] === $item[0] && $menu[1] === $item[1]) {
+            $menu[2] = array_merge($item[2], $menu[2]);
+            $menu[3] = array_merge($item[3], $menu[3]);
+            $menu[4] = $item[4];
+            return;
+        }
+    }
+});
+$aMenuLinks = $aMenuLinksExt;
