f29e4b8c28b3913f3e77aebf4f3983d66b7dc4ed
alex_mironov <alexan9610@gmail.com>
2022-01-18 23:29:06 +0000

    инициализация обработчика событий

--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -3,3 +3,12 @@ if (! defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) {die();}
 
 define('NO_IMAGE_PATH', '/local/templates/.default/vendor/images/no_image.png');
 define('SITE_TEMPLATE_DEFAULT', '/local/templates/.default');
+
+\Bitrix\Main\EventManager::getInstance()->addEventHandler('main', 'OnAfterUserLogin', function ($arParams) {
+    if ($arParams['USER_ID'] > 0) {
+        echo '<pre>';
+        var_dump($arParams);
+        echo '</pre>';
+        die;
+    }
+});
