c5850be26659ab0f94dcb12c606e5d5d0a568ee9
alex_mironov <alexan9610@gmail.com>
2022-01-19 00:43:10 +0000

    Доработка обработчика событий

--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -5,10 +5,17 @@ define('NO_IMAGE_PATH', '/local/templates/.default/vendor/images/no_image.png');
 define('SITE_TEMPLATE_DEFAULT', '/local/templates/.default');
 
 \Bitrix\Main\EventManager::getInstance()->addEventHandler('main', 'OnAfterUserLogin', function ($arParams) {
-    if ($arParams['USER_ID'] > 0) {
-        echo '<pre>';
-        var_dump($arParams);
-        echo '</pre>';
-        die;
+    if ($arParams['USER_ID'] > 0 && $requestUser = CUser::GetByID($arParams['USER_ID'])) {
+        $user = $requestUser->Fetch();
+        $arSend = [
+            'MESSAGE_ID' => '87',
+            'LID' => 's1',
+            'FIELDS' => [
+                'EMAIL' => $user['EMAIL'],
+                'LOGIN' => $user['LOGIN'],
+                'DATE' => $user['LAST_LOGIN'],
+            ],
+        ];
+        Bitrix\Main\Mail\Event::sendImmediate($arSend);
     }
 });
