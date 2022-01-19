4993dae1aa1e4ad3915ca7e050fd060f08acf443
alex_mironov <alexan9610@gmail.com>
2022-01-19 01:17:14 +0000

    Исправлены типовые ошибки

--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -4,18 +4,15 @@ if (! defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) {die();}
 define('NO_IMAGE_PATH', '/local/templates/.default/vendor/images/no_image.png');
 define('SITE_TEMPLATE_DEFAULT', '/local/templates/.default');
 
-\Bitrix\Main\EventManager::getInstance()->addEventHandler('main', 'OnAfterUserLogin', function ($arParams) {
-    if ($arParams['USER_ID'] > 0 && $requestUser = CUser::GetByID($arParams['USER_ID'])) {
-        $user = $requestUser->Fetch();
-        $arSend = [
-            'MESSAGE_ID' => '87',
-            'LID' => 's1',
-            'FIELDS' => [
-                'EMAIL' => $user['EMAIL'],
-                'LOGIN' => $user['LOGIN'],
-                'DATE' => $user['LAST_LOGIN'],
-            ],
-        ];
-        Bitrix\Main\Mail\Event::sendImmediate($arSend);
-    }
+\Bitrix\Main\EventManager::getInstance()->addEventHandler('main', 'OnAfterUserAuthorize', function ($arParams) {
+    $arSend = [
+        'EVENT_NAME' => 'USER_LOGIN',
+        'LID' => SITE_ID,
+        'FIELDS' => [
+            'EMAIL' => $arParams['user_fields']['EMAIL'],
+            'LOGIN' => $arParams['user_fields']['LOGIN'],
+            'DATE' => date('Y.m.d H:i:s'),
+        ],
+    ];
+    Bitrix\Main\Mail\Event::sendImmediate($arSend);
 });
