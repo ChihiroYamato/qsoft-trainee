7922ff786c29a7931b27e7b0aca951e07fe3ddfc
alex_mironov <alexan9610@gmail.com>
2022-01-22 20:19:17 +0000

    РЕализация шаблона компонента

---
+++ local/templates/.default/components/bitrix/news/news_qsoft/bitrix/news.list/news_list_inner/lang/en/template.php
@@ -0,0 +1,4 @@
+<?php
+$MESS['EDIT_LINK_TEXT'] = 'Edit news';
+$MESS['DELETE_LINK_TEXT'] = 'Delete news';
+$MESS['DELETE_CONFIRM'] = 'All the information linked to this record will be deleted. Continue anyway?';
