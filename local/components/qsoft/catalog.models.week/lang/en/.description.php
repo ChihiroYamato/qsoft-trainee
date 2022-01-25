91f2db936f0efbe723d8229c2da68e4c20d0553b
alex_mironov <alexan9610@gmail.com>
2022-01-25 17:30:10 +0000

    Инициализация основных файлов

---
+++ local/components/qsoft/catalog.models.week/lang/en/.description.php
@@ -0,0 +1,4 @@
+<?php
+$MESS['COMPONENT_NAME'] = 'Model week';
+$MESS['COMPONENT_DESC'] = 'Component for Model week';
+$MESS['COMPONENT_PATH_ID'] = "Custom";
---
+++ local/components/qsoft/catalog.models.week/lang/ru/.description.php
@@ -0,0 +1,4 @@
+<?php
+$MESS['COMPONENT_NAME'] = 'Модели недели';
+$MESS['COMPONENT_DESC'] = 'Компонент выводит Модели недели ИБ "Товары"';
+$MESS['COMPONENT_PATH_ID'] = 'Пользовательские';
